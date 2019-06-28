9bc9163c61a8201225f9e71d2e7d6807186da2df
stefan.bakmaz <stefan.bakmaz@vicert.com>
2019-06-28 19:27:50 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Ball from './src/Ball';
+import Deck from './src/Deck';
+import { Card, Button } from 'react-native-elements';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  renderCard = (item) => (
+    <Card
+      key={item.id}
+      title={item.text}
+      image={{ uri: item.uri }}
+    >
+      <Text style={{ marginBottom: 10 }}>I can customize the Card further</Text>
+      <Button
+        backgroundColor="#03A9F4"
+        title="View Now!"
+      />
+    </Card>
+  )
+
+  renderNoMoreCards = () => (
+    <Card title="All Done!">
+      <Text style={{ marginBottom: 10 }}>
+        There's no more content here!
+      </Text>
+      <Button
+        backgroundColor="#03A9F4"
+        title="Get more!"
+      />
+    </Card>
+  )
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
